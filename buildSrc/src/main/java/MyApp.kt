70a643ed16c3f8626c3c481044a67013b964e483
gaodeng <gaodengming@gmail.com>
2026-08-14 13:38:05 +0800

    Release version 1.0.6

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.bytemyth.craftecho.icons"
-    const val version = 6
-    const val versionName = "1.0.5"
+    const val version = 7
+    const val versionName = "1.0.6"
 }
