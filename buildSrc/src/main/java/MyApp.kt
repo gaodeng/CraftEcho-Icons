b26fa4f6199c7c294984bb336cbc70c9a35e2cbb
gaodeng <gaodengming@gmail.com>
2026-09-12 23:00:00 +0800

    Update TP-LINK IoT icon

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.bytemyth.craftecho.icons"
-    const val version = 20
-    const val versionName = "1.0.19"
+    const val version = 21
+    const val versionName = "1.0.20"
 }
